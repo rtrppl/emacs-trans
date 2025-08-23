297323e432a629f907d4fff7b1b4343e386af1b4
rtrppl <63909884+rtrppl@users.noreply.github.com>
2025-08-23 22:19:30 +0200

    adding polish

--- emacs-trans.el
+++ emacs-trans.el
@@ -1,3 +1,38 @@
+;; emacs-trans.el --- -*- lexical-binding: t -*-
+
+;; Maintainer: René Trappel <rtrappel@gmail.com>
+;; URL:
+;; Version: 0.1
+;; Package-Requires: emacs "26"
+;; Keywords: search web
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A wrapper for translate-shell.
+;;
+;;; News
+;;
+;; 0.1.
+;; - Initial release
+
+
+;;; Code:
+
 (defvar emacs-trans-show-alternatives "n")
 (defvar emacs-trans-show-prompt-message "n")
 (defvar emacs-trans-show-languages "n")
@@ -60,3 +95,5 @@
       (insert translation))
     (next-window)
     (display-buffer buffer)))
+
+;;; emacs-trans.el ends here
