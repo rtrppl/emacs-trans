95a50c79c70462d948ed867680bc37917080e522
rtrppl <63909884+rtrppl@users.noreply.github.com>
2025-09-26 15:11:55 +0200

    Added emacs-trans-line

--- emacs-trans.el
+++ emacs-trans.el
@@ -2,7 +2,7 @@
 
 ;; Maintainer: René Trappel <rtrappel@gmail.com>
 ;; URL:
-;; Version: 0.1
+;; Version: 0.1.2
 ;; Package-Requires: emacs "26"
 ;; Keywords: search web
 
@@ -23,9 +23,20 @@
 
 ;;; Commentary:
 
-;; A wrapper for translate-shell.
+;; An Emacs wrapper for translate-shell.
 ;;
 ;;; News
+;;
+;; 0.1.3
+;; - Added `emacs-trans-line'
+;;
+;; 0.1.2
+;; -Improved design of translation buffer (q=to kill this buffer);
+;; small fixes
+;;
+;; 0.1.1
+;; - Added =emacs-trans-url= to open a URL at point (or otherwise
+;;  provided) in Google Translate via `translate-shell'; minor fixes
 ;;
 ;; 0.1.
 ;; - Initial release
@@ -35,12 +46,12 @@
 (require 'org) ; for grabbing the link in an org-document
 (require 'shr) ; for grabbing the link in an non-org-document
 
-
 (defvar emacs-trans-show-alternatives "n")
 (defvar emacs-trans-show-prompt-message "n")
 (defvar emacs-trans-show-languages "n")
 (defvar emacs-target-language ":en") ; if nil, default will be used
 
+;;;###autoload
 (defun emacs-trans-next-sentence ()
  "Translates the next sentence using translate-shell."
  (interactive)
@@ -53,6 +64,20 @@
    (deactivate-mark)
    (emacs-trans-open-results-buffer translation)))
 
+;;;###autoload
+(defun emacs-trans-line (&optional minimal)
+  "Translates the current line using translate-shell."
+  (interactive)
+  (let* ((line (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+	 (trans-cmd (emacs-trans-create-trans-cmd))
+	 (translation))
+    (when (not minimal)
+      (setq translation (emacs-trans-translate trans-cmd line))
+      (emacs-trans-open-results-buffer translation))
+    (when minimal
+      (setq trans-cmd (emacs-trans-create-trans-cmd t))
+      (setq translation (emacs-trans-translate trans-cmd line t)))))
+
 (defun emacs-trans-input ()
  "Translates an the next sentence using translate-shell."
  (interactive)
@@ -61,16 +86,27 @@
 	(translation (emacs-trans-translate trans-cmd input)))
    (emacs-trans-open-results-buffer translation)))
 
-(defun emacs-trans-region ()
+(defun emacs-trans-region (&optional string)
  "Translates the active region."
  (interactive)
- (let* ((region (buffer-substring-no-properties (region-beginning) (region-end)))
+ (let* ((region (or
+		 string
+		 (buffer-substring-no-properties (region-beginning) (region-end))))
 	(region (replace-regexp-in-string "\n" " " region))
 	(trans-cmd (emacs-trans-create-trans-cmd))
-	(translation (emacs-trans-translate trans-cmd region)))
-   (emacs-trans-open-results-buffer translation)))
+	(translation))
+   (when (not string)
+     (setq translation (emacs-trans-translate trans-cmd region))
+     (emacs-trans-open-results-buffer translation))
+   (when string
+     (setq trans-cmd (emacs-trans-create-trans-cmd t))
+     (setq translation (emacs-trans-translate trans-cmd region)))
+   translation))
+
+
 	
-(defun emacs-trans-create-trans-cmd ()
+;;;###autoload
+(defun emacs-trans-create-trans-cmd (&optional minimal)
   "Creates the correct cmd for translate-shell."
   (let ((cmd "trans"))
     (when emacs-target-language
@@ -81,6 +117,8 @@
       (setq cmd (concat cmd " -show-prompt-message n")))
     (when (string-equal emacs-trans-show-languages "n")
       (setq cmd (concat cmd " -show-languages n")))
+    (when minimal
+      (setq cmd (concat "trans " emacs-target-language " -b ")))
     cmd))
 
 (defun emacs-trans-translate (cmd string)
@@ -97,9 +135,22 @@
   (let ((buffer (get-buffer-create "*emacs-trans*")))
     (with-current-buffer buffer
       (erase-buffer)
-      (insert translation))
-    (next-window)
+      (insert "(q=kill this buffer)\n\n")
+      (insert translation)
+      (emacs-trans-results-buffer-mode 1))
     (display-buffer buffer)))
+
+(define-minor-mode emacs-trans-results-buffer-mode
+  "A minor mode for emacs-trans results buffers."
+  :lighter " emacs-trans-results-buffer"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "q") 'emacs-trans-close-buffer)
+            map))
+
+(defun emacs-trans-close-buffer ()
+   "A wrapper to close BUFFER. "
+   (interactive)
+   (kill-buffer))
 
 ;;;###autoload
 (defun emacs-trans-url (&optional url dummy)
